0a878a323d8469e90233d361970bfa30c6861aa4
DC-VIETIS\ducnhm <123456d@>
2022-05-09 09:31:31 +0700

    Update test

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div id="app">
+  <v-app id="app">
     <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </nav>
-    <router-view/>
-  </div>
+    <router-view />
+  </v-app>
 </template>
 
 <style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <v-img
+      max-width="100%"
+      max-height="100vh"
+      lazy-src="https://picsum.photos/id/11/10/6"
+      src="https://scontent.fhan2-4.fna.fbcdn.net/v/t39.30808-6/262378042_956834808565157_9194212351594827181_n.jpg?_nc_cat=103&ccb=1-6&_nc_sid=8bfeb9&_nc_ohc=q6bt9g1cHy8AX9BL8gw&tn=4s-IMhxhexSXyYfc&_nc_ht=scontent.fhan2-4.fna&oh=00_AT_nPVFb9m9a0KQq7Bpsg-_oM9oP2CzPLS8QFYcfDkS44g&oe=627DC3AD"
+    ></v-img>
   </div>
 </template>
